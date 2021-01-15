051057e27fad433a134f3d103c90b67952cb84f6
unifare <aion888@126.com>
2021-01-15 15:49:40 +0800

    Update Config.cs
    add username password mode

--- Ids4Center.Mvc/Config.cs
+++ Ids4Center.Mvc/Config.cs
@@ -27,6 +27,30 @@ namespace Ids4Center.Mvc
                     // 该客户端可以访问的资源集合
                     AllowedScopes = { "one-api" }
                 },
+                new Client
+                {
+                    ClientId = "client_b",
+                    AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
+                    //AccessToken过期时间(秒),默认为3600秒/1小时
+                    AccessTokenLifetime=3600,
+
+                    //RefreshToken的最长生命周期
+                    //AbsoluteRefreshTokenLifetime = 2592000,
+
+                    //RefreshToken生命周期以秒为单位。默认为1296000秒
+                    SlidingRefreshTokenLifetime = 2592000,//以秒为单位滑动刷新令牌的生命周期。
+
+                    //刷新令牌时，将刷新RefreshToken的生命周期。RefreshToken的总生命周期不会超过AbsoluteRefreshTokenLifetime。
+                    RefreshTokenExpiration = TokenExpiration.Sliding,
+
+                    //AllowOfflineAccess 允许使用刷新令牌的方式来获取新的令牌
+                    AllowOfflineAccess = true,
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+                    AllowedScopes = { "one-api" }
+                }
             };
         }
 
